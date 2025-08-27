00eb4803095bc94f6495e97a552b2a461abf6b68
SaxenaNikhil <nikhilsaxena97@gmail.com>
2025-08-26 22:42:28 -0400

    DSA Blind 75 Arrays and Hashing

--- DSA/1-two-sum.py
+++ DSA/1-two-sum.py
@@ -3,15 +3,30 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}  # stores number: index
+        values = dict()
 
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in hashmap:
-                return [hashmap[complement], i]
-            hashmap[num] = i
+        for i, elem in enumerate(nums):
+            complement = target - elem
+
+            if complement in values:
+                return [values[complement], i] # To find the complement index inside the values array and i is the current index value.
+
+            values[elem] = i # Storing the current element from nums inside the values dictionary for the current index where elem is key and i is values.
+
         return []
+
+
+if __name__ == "__main__":
+    sol = Solution()
 
     nums1 = [2, 7, 11, 15]
     target1 = 9
-    print("Result:", twoSum(nums1, target1))  # [0, 1]
+    print("Result:", sol.twoSum(nums1, target1))  # [0, 1]
+
+    nums2 = [3, 2, 4]
+    target2 = 6
+    print("Result:", sol.twoSum(nums2, target2))  # [1, 2]
+
+    nums3 = [3, 3]
+    target3 = 6
+    print("Result:", sol.twoSum(nums3, target3))  # [0, 1]
